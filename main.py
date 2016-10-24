bb8266653623664aaf1888942d9e564df89a4bc6
Kevin Higgins <kevin@voltisoft.com>
2016-10-24 17:34:03 -0600

    update

--- main.py
+++ main.py
@@ -92,4 +92,5 @@ def postreply(b,ident):
 	return redirect('/boards/%s/threads/%s' % (str(b),str(ident)))
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
