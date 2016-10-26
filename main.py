b3c8b40a0833dd85ce7eff169e425891f3dea76e
Kevin Higgins <kevin@voltisoft.com>
2016-10-26 14:52:13 -0600

    postgresql fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sql = SQLAlchemy(app)
 #c = sql.cursor()
 c = sql.engine
 c.execute("CREATE TABLE IF NOT EXISTS threads(name VARCHAR(100), post VARCHAR(500),id INT, board VARCHAR(20))")
-c.execute("CREATE TABLE IF NOT EXISTS posts(name VARCHAR(100), post VARCHAR(500), id INT, board VARCHAR(20), parent VARCHAR(100))")
+c.execute("CREATE TABLE IF NOT EXISTS posts(name VARCHAR(100), post VARCHAR(500), id INT, board VARCHAR(20), parent INT)")
 c.execute("CREATE TABLE IF NOT EXISTS boards(name VARCHAR(100), postcount INT, description VARCHAR(100))")
 c.execute("ALTER TABLE boards ALTER COLUMN postcount SET DEFAULT 0")
 boards = c.execute("SELECT name FROM boards").fetchall()
