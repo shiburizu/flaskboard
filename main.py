d220ba46b2deb22099a2f45dbbb4aaed939f043e
Kevin Higgins <kevin@voltisoft.com>
2016-10-27 18:10:53 -0600

    postgresql prefixing

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def showboard(ident):
 		for i in board:
 			realboard.append(i[0])
 			realboard.append(i[1].replace('$_FLASKBOARD_CONTENT$',''))
-		posts = g.db.execute("SELECT name,post FROM threads WHERE board = %s" % realident).fetchall()
+		posts = g.db.execute("SELECT name,post FROM threads WHERE board = '%s'" % realident).fetchall()
 		realpost = []
 		for i in posts:
 			realpost.append(i[0].replace('$_FLASKBOARD_CONTENT$',''))
